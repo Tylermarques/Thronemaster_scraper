6e9d9d6128019b6a6fa9ba3ded81c0bb6fb1dbbb
Tyler Marques <tmarques@shoplogix.com>
2018-03-08 12:34:33 -0500

    commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-engine = create_engine('postgresql://postgres@localhost/game_of_thrones')
+
+engine = create_engine(f'postgresql://craig:thronemaster@10.50.50.132/game_of_thrones')
 Session = sessionmaker(bind=engine)
 
 
