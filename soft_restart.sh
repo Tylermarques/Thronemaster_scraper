6fb96411a9d64e5dda4c60c57af0553bed59bbf1
Tyler Marques <tmarques@shoplogix.com>
2018-03-14 16:25:48 -0400

    Automated commit message

--- soft_restart.sh
+++ soft_restart.sh
@@ -4,6 +4,7 @@ git add .
 git commit -m "Automated commit message"
 
 ssh tmarques:Lee_c1aa@10.50.50.132 << EOF
+  cd ~/Documents/Projects/Thronemaster_scraper/
   git fetch --all
   git reset --hard origin/master
 EOF
